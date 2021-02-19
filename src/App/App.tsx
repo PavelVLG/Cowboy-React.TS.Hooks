f5838229db8c5ed0fb7cd336487d5a263b1ee433
PavelVLG <pavelvlg4@mail.ru>
2021-02-19 20:34:53 +0300

    master

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Header} from './Component/Header/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p>Heloo TS</p>
+       <p><Header/></p>
       </header>
     </div>
   );
 }
 
 export default App;
+/* значит проект о садиках. Какие садики рядом, оценка , расстоняие до школы/альтернативные маршруты/ посмотреть маршрут/ расписание занятий*/
---
+++ src/App/Component/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const Header: React.FC = () => {
+
+    return(
+    <div className="wrapper"><div className="wrapperFlex">
+        <div className="name">Найти садики</div>
+        <div className="rating">Рейтинг</div>
+        <div className="route">Маршрут</div>
+    </div></div>)
+
+};
