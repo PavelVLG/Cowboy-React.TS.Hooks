28c84822f7a9fbd2c2357835e32243f94296789f
PavelVLG <pavelvlg4@mail.ru>
2021-02-20 11:04:46 +0300

    master

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import './App.css';
-import {Header} from './Component/Header/Header';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <p><Header/></p>
+       Что то
       </header>
     </div>
   );
 }
 
 export default App;
-/* значит проект о садиках. Какие садики рядом, оценка , расстоняие до школы/альтернативные маршруты/ посмотреть маршрут/ расписание занятий*/
