2d66b0a520e709cd1c1876c37f336ce1ee6694cc
PavelVLG <pavelvlg4@mail.ru>
2021-02-20 12:09:05 +0300

    master

--- src/Component/MainWrapper/MainWrapper.tsx
+++ src/Component/MainWrapper/MainWrapper.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import './MainWrapper.css';
-import { LevelChange } from '../Element/LevelChange/LevelChange';
-import { Output } from '../Element/Output/Output';
-import { PopUp } from '../Element/PopUp/PopUp';
-import { StartButton } from '../Element/StartButton/StartButton';
-import { SubTitle } from '../Element/SubTitle/SubTitle';
-import { TitleHeader } from '../Element/TitleHeader/TitleHeader';
-export const MainWrapper = () => {
+import React from "react";
+import "./MainWrapper.css";
+import { LevelChange } from "../Element/LevelChange/LevelChange";
+import { Output } from "../Element/Output/Output";
+import { PopUp } from "../Element/PopUp/PopUp";
+import { StartButton } from "../Element/StartButton/StartButton";
+import { SubTitle } from "../Element/SubTitle/SubTitle";
+import { TitleHeader } from "../Element/TitleHeader/TitleHeader";
 
-    return (<>
-        <LevelChange />
-        <Output />
-        <PopUp />
-        <StartButton />
-        <SubTitle />
-        <TitleHeader />
-    </>)
-}
+export const MainWrapper = () => {
+  return (
+    <>
+      <LevelChange />
+      <Output />
+      <PopUp />
+      <StartButton />
+      <SubTitle />
+      <TitleHeader />
+    </>
+  );
+};
