c47fd32cba826ca29db3060ad2f6e373ecf5b812
PavelVLG <pavelvlg4@mail.ru>
2021-02-21 12:43:16 +0300

    master

--- src/Component/Element/StartButton/StartButton.tsx
+++ src/Component/Element/StartButton/StartButton.tsx
@@ -1,5 +1,13 @@
 import './StartButton.css';
 
 export const StartButton= () => {
-    return <div className="startButton"> StartButton </div>;
-};
+    return (
+        <div className="start">
+            <div className="startConteiner">
+                <button
+                    className="startButton"
+                    type="button"
+                    onClick={(e)=> console.log(e.target) }
+                >Старт</button></div>
+        </div>)
+}; 
