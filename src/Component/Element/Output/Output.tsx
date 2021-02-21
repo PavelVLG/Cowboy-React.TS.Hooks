fe700bf1c7915570bd9b8c5993863fab0ad5dfeb
PavelVLG <pavelvlg4@mail.ru>
2021-02-21 12:40:24 +0300

    master

--- src/Component/Element/Output/Output.tsx
+++ src/Component/Element/Output/Output.tsx
@@ -1,5 +1,22 @@
 import "./Output.css";
 
 export const Output = () => {
-  return <div className="outPut">Output</div>;
+  return (
+    <div className="exitConteiner">
+      <p className="exit-befor">
+        {" "}
+        .class &ensp;&#123; <br />
+        display: flex;{" "}
+      </p>
+      <textarea
+        className="exitInput"
+        placeholder="поле ввода"
+        onChange={(e) => {
+          console.log(e.target.value, "OutPut");
+        }}
+        id="txtarea"
+      ></textarea>
+      <p className="exit-after"> &#125;</p>
+    </div>
+  );
 };
