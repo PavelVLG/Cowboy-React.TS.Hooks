1248738ab1506e20fb0bdc1d40390fc5e4781ef2
PavelVLG <pavelvlg4@mail.ru>
2021-02-21 12:34:16 +0300

    master

--- src/Component/Element/LevelChange/LevelChange.tsx
+++ src/Component/Element/LevelChange/LevelChange.tsx
@@ -1,5 +1,26 @@
 import "./LevelChange.css";
 
 export const LevelChange = () => {
-  return <div className="levelChage">LevelChange</div>;
+  return (
+    <div className="textButton-conteiner">
+      <div className="textFlex">
+        <div className="buttonText">
+          <p>Уровень </p>
+        </div>
+        <div className="battonItem">
+          <div>
+            <button className="button-1" type="button" onClick={()=> console.log("button--1")}>
+              назад
+            </button>
+          </div>
+          <div>
+            <button className="button-2" type="button" onClick={()=> console.log("button--2")}>
+              вперед
+            </button>
+          </div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 };
