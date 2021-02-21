fbfdf200f4f27a06236dceca42a3ed057e17ad60
PavelVLG <pavelvlg4@mail.ru>
2021-02-21 11:15:36 +0300

    master

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,5 +1,20 @@
 import React from "react";
-
+import "./Header.css";
 export const Header: React.FC = () => {
-  return <div className="header">Header</div>;
+  return (
+    <nav className="header">
+      <div className="headerLogo">
+        <p>
+          Pavel<span className="logoSpan">VLG</span>4
+        </p>
+      </div>
+      <div className="logoSpan">
+        <div className="headerItem">
+          <button className="headerBtn" type="button">
+            Включить атмосферу
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
