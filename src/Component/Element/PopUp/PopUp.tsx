e4c6eb8cf3f4763ba93bf3e041c788d62c6a6f09
PavelVLG <pavelvlg4@mail.ru>
2021-02-21 12:47:15 +0300

    master

--- src/Component/Element/PopUp/PopUp.tsx
+++ src/Component/Element/PopUp/PopUp.tsx
@@ -1,5 +1,17 @@
 import "./PopUp.css";
 
 export const PopUp = () => {
-  return <div className="popUp"> PopUp </div>;
+  return (
+    <div className="modal">
+      <div className="modalContent">
+        <div className="modalText">
+          <p>
+            <span>Джо cбежал</span>
+          </p>
+        </div>
+      </div>
+      {/* изначально план был что бы модальное окно появлялось в зависимости от props и по клику закрывалось,  делал через класс и менял стейт в зависимости от пропса но получалось зацикливание, в текущем положении тоже не получилось, сделал окно без кнопки через timer в props, еще пробовал компоненты жизненного цикла, тоде не вышло. Так как я хотел сделать основную логику popUp здесь, то решил не добавлять новых state родительскому компоненту для реализации закрытия. */}
+      {/* <div className="modal__close"><button className="close__btn" onClick={() => test()}>X</button></div> */}
+    </div>
+  );
 };
