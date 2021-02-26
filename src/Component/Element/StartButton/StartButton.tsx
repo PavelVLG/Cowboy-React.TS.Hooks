160a2752f452207b92b65a13a6f9a459ac9a082f
PavelVLG <pavelvlg4@mail.ru>
2021-02-26 11:02:08 +0300

    master

--- src/Component/Element/StartButton/StartButton.tsx
+++ src/Component/Element/StartButton/StartButton.tsx
@@ -1,17 +1,19 @@
+import React from "react";
+import { IData } from "../Interfaces/Interfaces";
 import "./StartButton.css";
 
-export const StartButton: React.FC = () => {
-    return (
-        <div className="start">
-          <div className="startConteiner">
-            <button
-              className="startButton"
-              type="button"
-              onClick={() => console.log()}
-            >
-              Старт
-            </button>
-          </div>
-        </div>
+export const StartButton: React.FC<IData> = () => {
+  return (
+    <div className="start">
+      <div className="startConteiner">
+        <button
+          className="startButton"
+          type="button"
+          onClick={() => console.log()}
+        >
+          Старт
+        </button>
+      </div>
+    </div>
   );
 };
