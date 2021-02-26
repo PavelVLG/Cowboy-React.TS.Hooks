a835f2cb55afe73ebc5989f30fed3ec7f1fab375
PavelVLG <pavelvlg4@mail.ru>
2021-02-26 10:57:23 +0300

    master

---
+++ src/Component/Element/Interfaces/Interfaces.ts
@@ -0,0 +1,8 @@
+export interface IData {
+  data: object;
+  introductory: string;
+  askDescription: string;
+  task: { key: object; value: string };
+  infoElement: [];
+  locationCells: { key: object; value: string };
+}
