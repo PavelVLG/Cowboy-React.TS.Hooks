11336e0acad0712361c6d31c378fcd1cf05a7853
PavelVLG <pavelvlg4@mail.ru>
2021-02-28 15:27:23 +0300

    master

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from "react";
 import "./Header.css";
-
+import  Music  from "../Source/Audo/ugly.mp3";
 export const Header: React.FC = () => {
-  const [volume, setVolume] = useState<boolean>(true);
+  const [volume, setVolume] = useState<boolean>(false);
+
+  useEffect(() => {
+    const audioPlay: HTMLAudioElement = new Audio(Music);
+    if (volume) {
+      console.log("play");
+      audioPlay.play();
+    } else {
+      console.log("not play");
+      audioPlay.pause();
+    }
+  }, [volume]);
 
   const sound = () => {
     let sound: string = "mute";
-    if (volume) {
+    if (!volume) {
       sound = "volume";
     }
     return sound;
   };
+
   const test = (): any => {
     setVolume((): any => {
       setVolume(!volume);
@@ -22,7 +34,7 @@ export const Header: React.FC = () => {
         <p>PavelVLG4</p>
       </div>
       <label className="lable" htmlFor="">
-        <input className="checkbox" type="checkbox" ></input>
+        <input className="checkbox" type="checkbox"></input>
         <span className={sound()} onClick={test}></span>
       </label>
     </nav>
