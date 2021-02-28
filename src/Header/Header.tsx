eb009faf3e0f7fb2f2bf27bb842844d14a3b887c
PavelVLG <pavelvlg4@mail.ru>
2021-02-28 17:46:38 +0300

    master

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,18 +1,14 @@
 import React, { useState, useEffect } from "react";
 import "./Header.css";
 import Music from "../Source/Audo/ugly.mp3";
-
 const audioPlay: HTMLAudioElement = new Audio(Music);
-
 export const Header: React.FC = () => {
   const [volume, setVolume] = useState<boolean>(false);
 
   useEffect(() => {
     if (volume) {
-      console.log("play");
       audioPlay.play();
     } else {
-      console.log("not play");
       audioPlay.pause();
     }
   }, [volume]);
