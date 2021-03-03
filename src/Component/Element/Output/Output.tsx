138824f70bbc6c8026d8c680fc1b0909af6b1479
PavelVLG <pavelvlg4@mail.ru>
2021-03-03 18:42:13 +0300

    master

--- src/Component/Element/Output/Output.tsx
+++ src/Component/Element/Output/Output.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import "./Output.css";
 
 interface ProppsFunc {
-  getEvent: (e: any) => void;
+  getEvent: (a: string) => void;
 }
 
 export const Output: React.FC<ProppsFunc> = ({ getEvent }) => {
   return (
     <div className="exitConteiner">
       <p className="exit-befor">
-        {" "}
         .class &ensp;&#123; <br />
-        display: flex;{" "}
+        display: flex;
       </p>
       <textarea
         className="exitInput"
